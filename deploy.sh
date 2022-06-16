069325dd8ad806521339d2f7e875414712233429
ericxlee <ericxlee@foxmail.com>
2022-06-16 16:15:50 +0800

    asdad

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env shx
 
-# 当发生错误时中止脚本
+#当发生错误中止脚本
 set -e
-
-# 构建
-npm run build:prod
-
 # cd 到构建输出的目录下
 cd dist
-
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
+# 这里的access_token是travis设置的环境变量
+git push -f https://${gtoken}@github.com/jieye-ericx/conference-submission-system master:deploy
+cd -  # 返回上一层
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jieye-ericx/Conference-submission-system.git master:gh-pages
-
-cd -
+# ssh root@47.103.212.239 'Hx608136 && yes && cd /paperSubmission/frontend_travis/ && mkdir aaa'
---
+++ ttt.sh
@@ -0,0 +1,2 @@
+
+echo 'Hx608136' | scp  -o stricthostkeychecking=no -r ./dist/* root@47.103.212.239:/paperSubmission/frontend_travis/  # 使用scp拷贝到服务
