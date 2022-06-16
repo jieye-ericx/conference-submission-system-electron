e78ce75403cf49b0b8a9f15c10b5a3a9008eac53
ericxlee <ericxlee@foxmail.com>
2022-06-16 16:36:35 +0800

    aaa

--- ttt.sh
+++ ttt.sh
@@ -1,2 +1,16 @@
-
-echo 'Hx608136' | scp  -o stricthostkeychecking=no -r ./dist/* root@47.103.212.239:/paperSubmission/frontend_travis/  # 使用scp拷贝到服务
+#!/usr/bin/expect
+# echo 'Hx608136' | scp  -o stricthostkeychecking=no -r ./dist/* root@47.103.212.239:/paperSubmission/frontend_travis/  # 使用scp拷贝到服务
+spawn scp  -o stricthostkeychecking=no -r ./dist/* root@47.103.212.239:/paperSubmission/frontend_travis/
+ expect {
+ "(yes/no)?"
+  {
+    send "yes\n"
+    expect "*assword:" { send "Hx608136\n"}
+  }
+ "*assword:"
+  {
+    send "Hx608136\n"
+  }
+}
+expect "100%"
+expect eof
