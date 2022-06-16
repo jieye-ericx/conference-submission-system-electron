8c02c2f1faf8b48f027ac262bda30a01a0a58cf1
ericxlee <ericxlee@foxmail.com>
2022-06-16 16:40:39 +0800

    a

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
       <div class="title-container">
-        <h3 class="title">期刊投稿系统登陆</h3>
+        <h3 class="title">期刊投稿系统</h3>
       </div>
       <el-form-item prop="role">
         <span class="svg-container">
